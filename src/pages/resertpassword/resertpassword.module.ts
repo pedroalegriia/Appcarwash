8414ec9d1e5074e84103c21ac0f5db6fddc8fae0
pedroalegriia <pedro_alegriia@outlook.com>
2018-09-10 01:17:15 -0500

    app para autolavado

---
+++ src/pages/resertpassword/resertpassword.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ResertpasswordPage } from './resertpassword';
+
+@NgModule({
+  declarations: [
+    ResertpasswordPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ResertpasswordPage),
+  ],
+})
+export class ResertpasswordPageModule {}
---
+++ src/pages/resertpassword/resertpassword.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ResertpasswordPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-resertpassword',
+  templateUrl: 'resertpassword.html',
+})
+export class ResertpasswordPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ResertpasswordPage');
+  }
+
+}
